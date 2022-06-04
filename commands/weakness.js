663512598e79995fa5e86f919157c3edc2f4e610
ARLPLAYZ <98569491+ARLPLAYZ@users.noreply.github.com>
2022-06-04 09:15:21 +0530

    Update weakness.js

--- commands/weakness.js
+++ commands/weakness.js
@@ -1,5 +1,25 @@
 const { MessageEmbed } = require('discord.js');
 const weaknesses = require('../weaknesses');
+const color = {
+    normal: 0x9B9B6B,
+    fire: 0xE5711E,
+    water: 0x4C7BED,
+    electric: 0xF2C617,
+    grass: 0x69B741,
+    ice: 0x7FCECE,
+    fighting: 0xAF2C25,
+    poison: 0x8E398E,
+    ground: 0xD9B34A,
+    flying: 0x9C88DA,
+    psychic: 0xF7356F,
+    bug: 0x9BA91E,
+    rock: 0xA48F32,
+    ghost: 0x634E86,
+    dragon: 0x6124F5,
+    dark: 0x5E493C,
+    style: 0xA6A6C4,
+    fairy: 0xE484E4,
+};
 module.exports = {
     name: 'weakness',
     description: 'Get weaknesses of pokemon',
